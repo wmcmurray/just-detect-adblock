4bf69a07d5d1b58d991985d353bc51b2a6f3c684
William McMurray <tiwilinlove@hotmail.com>
2020-10-17 23:37:54 -0400

    Fix typo

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ module.exports = {
     // create the bait
     var bait = document.createElement('div');
     bait.setAttribute('class', 'pub_300x250 pub_300x250m pub_728x90 text-ad textAd text_ad text_ads text-ads text-ad-links');
-    bait.setAttribute('style', 'width: 1px ! important; height: 1px !important; position: absolute !important; left: -10000px !important; top: -1000px !important;');
+    bait.setAttribute('style', 'width: 1px !important; height: 1px !important; position: absolute !important; left: -10000px !important; top: -1000px !important;');
     window.document.body.appendChild(bait);
 
     // check if the bait has been affected by an adblocker
