f6a91cf0c4b10e1559073945a6ba30f0fee37829
William McMurray <tiwilinlove@hotmail.com>
2020-10-17 23:47:43 -0400

    Previous bait classes where not working anymore, as found out by @guanzo

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ module.exports = {
 
     // create the bait
     var bait = document.createElement('div');
-    bait.setAttribute('class', 'pub_300x250 pub_300x250m pub_728x90 text-ad textAd text_ad text_ads text-ads text-ad-links');
+    bait.setAttribute('class', 'pub_300x250 pub_300x250m pub_728x90 text-ad textAd text_ad text_ads text-ads text-ad-links ad-text adSense adBlock adContent adBanner');
     bait.setAttribute('style', 'width: 1px !important; height: 1px !important; position: absolute !important; left: -10000px !important; top: -1000px !important;');
     window.document.body.appendChild(bait);
 
